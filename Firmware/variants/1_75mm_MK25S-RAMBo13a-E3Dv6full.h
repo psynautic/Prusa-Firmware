dafb06a29c0ba73d9c6571535364791c28f42427
psynautic <psynautic@gmail.com>
2021-03-15 04:40:11 -0400

    setup use for PT1000 from trainglelab

--- Firmware/variants/1_75mm_MK25S-RAMBo13a-E3Dv6full.h
+++ Firmware/variants/1_75mm_MK25S-RAMBo13a-E3Dv6full.h
@@ -452,7 +452,7 @@
 #elif defined(E3D_PT100_EXTRUDER_NO_AMP)
 #define TEMP_SENSOR_0 148
 #else
-#define TEMP_SENSOR_0 5
+#define TEMP_SENSOR_0 1047  //  PT1000 from triangle labs no resistor swaps
 #endif
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
